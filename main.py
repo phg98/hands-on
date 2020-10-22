d61d22a87492bcd99c14bc92760059286c3de657
Harry Park <phg98@naver.com>
2020-10-22 23:33:35 +0900

    Add index.md

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+
+f = open("index.md", 'w')
+for i in range(1, 21):
+    data = "[{0}]({1}.md)".format(i, i)
+    f.write(data)
+f.close()
+exit()
+
+
 for i in range(1, 21):
     f = open(str(i)+".md", 'w')
     data = "[Hactoberfest Seoul Meetup!](https://event-us.kr/hacktoberfestseoul/event/23432)"
